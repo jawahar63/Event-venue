8ecf385291068da4dd108d321c7a99e11427f75d
jawahar63 <d.jawahar6382@gmail.com>
2024-10-28 12:08:49 +0530

    change

--- src/app/sheet.service.ts
+++ src/app/sheet.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { interval, map, Observable, switchMap } from 'rxjs';
+import { interval, map, Observable, switchMap, timer } from 'rxjs';
 import { environment } from './environments/environment.development';
 
 @Injectable({
@@ -14,10 +14,9 @@ export class SheetService {
     return this.http.get(this.sheetUrl);
   }
   getDataWithPolling(): Observable<Event[]> {
-    return interval(5000)
-      .pipe(
-        switchMap(() => this.getSheetData()),
-      );
+    return timer(0, 5000).pipe(
+      switchMap(() => this.getSheetData()),
+    );
   }
 
 }
